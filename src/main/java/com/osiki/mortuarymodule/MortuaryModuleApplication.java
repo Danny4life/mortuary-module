6932c8053553db1dcbe1b1fdeb5db260779c5d58
Segun Osiki <osiki.segun@gmail.com>
2023-07-07 11:46:19 +0100

    initial commit

---
+++ src/main/java/com/osiki/mortuarymodule/MortuaryModuleApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.mortuarymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortuaryModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortuaryModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/mortuarymodule/MortuaryModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.mortuarymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortuaryModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
